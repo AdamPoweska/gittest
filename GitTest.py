840f51a489d22cd810a5d08ee4f7b2ea51dd7da6
Adam <166754975+AdamPoweska@users.noreply.github.com>
2024-10-13 23:22:14 +0200

    creating branch on github website

--- GitTest.py
+++ GitTest.py
@@ -4,3 +4,4 @@
 # and I'll add some new lines
 # Now I'm making change to check Git Pull option
 # Line to check how changes are pushed to GitHUB.
+# and new line to check how works adding branches from GitHub website.
