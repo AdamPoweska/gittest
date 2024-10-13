b8fae6f965de5ae0900e92df289206b3effa5c26
Adam <166754975+AdamPoweska@users.noreply.github.com>
2024-10-13 22:23:55 +0200

    Update GitTest.py

--- GitTest.py
+++ GitTest.py
@@ -1,3 +1,4 @@
 #Test file to learn git.
 #New test line.
-# Now i want to add different lines to check if there will be a conflictt.
+# Now i want to add different lines to check if there will be a conflict.
+# and I'll add some new lines
